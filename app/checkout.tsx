c13fa841216fd9392a1a9764dbc4485a820eb9c4
riolly <alberto.riolly@gmail.com>
2022-11-07 21:33:44 +0700

    style: title font semi

--- app/checkout.tsx
+++ app/checkout.tsx
@@ -15,13 +15,13 @@ export default function CheckoutCard() {
 	return (
 		<div className="flex flex-col bg-white px-5 pt-4 pb-2 rounded-lg h-fit">
 			<div className="flex justify-between items-end py-4">
-				<p className="text-sky-900">YOUR BOOKING</p>
+				<p className="text-sky-900 font-medium">YOUR BOOKING</p>
 				<a className="text-sm text-sky-500">Details</a>
 			</div>
 
 			<div className="border-b-2 border-dashed w-full h-0.5" />
 			<div className="flex flex-col py-4 gap-2">
-				<p>LIST OF PASSENGER(S)</p>
+				<p className="font-medium">LIST OF PASSENGER(S)</p>
 				<div className="flex justify-between">
 					<p>{data.name}</p>
 					<p>{data.maturity}</p>
@@ -30,7 +30,7 @@ export default function CheckoutCard() {
 
 			<div className="border-b-2 w-full h-0.5" />
 			<div className="py-4 flex flex-col gap-2">
-				<p>FLIGHT</p>
+				<p className="font-medium">FLIGHT</p>
 				<span className="flex gap-2 items-center">
 					<Flight />
 					<p>
@@ -48,7 +48,7 @@ export default function CheckoutCard() {
 
 			<div className="border-b-2 w-full h-0.5" />
 			<div className="py-4 flex flex-col">
-				<p>PRICE</p>
+				<p className="font-medium">PRICE</p>
 				<div className="flex justify-between items-end">
 					<p>Total</p>
 					<span className="flex gap-2 items-center">
